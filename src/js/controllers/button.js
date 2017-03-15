d34fc1daba77728b91f22cac87fc734a13f50abb
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-14 21:31:58 -0400

    finished second button and decrement function within Button Controller

--- src/js/controllers/button.js
+++ src/js/controllers/button.js
@@ -11,6 +11,39 @@ function ButtonController ($scope) {
           $scope.val2 = "likes"
         }
     }
+    $scope.countDown = function () {
+      $scope.val -= 1;
+      console.log($scope.val)
+      if ($scope.val === 1) {
+        $scope.val2 = "like";
+      } else {
+          $scope.val2 = "likes"
+        }
+    }
+    // $scope.toggle = function () {
+    //
+    //   console.log($scope.val)
+    //   if ($scope.val === 1) {
+    //     $scope.val2 = "like";
+    //   } else {
+    //       $scope.val2 = "likes"
+    //     }
+    // }
+
+
+// functitoggle() ($scope) {
+//
+//     if countUp
+//     $scope.countUp = function () {
+//       $scope.val += 1;
+//       console.log($scope.val)
+//       if ($scope.val === 1) {
+//         $scope.val2 = "like";
+//       } else {
+//           $scope.val2 = "likes"
+//         }
+//     }
+// }
 }
 
 ButtonController.$inject = ['$scope'];
